51440d090e5a080c6194cb6c527f6e53423ea472
jd95 <jeffreydwyer95@hotmail.com>
2015-04-27 19:20:17 -0700

    Yet again...

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -89,6 +89,13 @@ convertStatement (Assignment [(Token SYMBOL var)] ((Token PUNCTUATION "["):(Toke
         accessArray = "lw " ++var++", 0($t2)"
     Just ([loadAddress] ++ loadindex ++ [accessArray])
 
+convertStatement (Assignment [(Token SYMBOL var)] ((Token PUNCTUATION "["):(Token SYMBOL name):(Token SYMBOL index):(Token PUNCTUATION "]"):[])) = do
+    let loadAddress = "la $t0, "++name
+        loadindex = ["move $t1, "++index] ++ ["add $t1, $t1, $t1"] ++ ["add $t1, $t1, $t1"] ++ ["add $t2, $t0, $t1"]
+        accessArray = "lw " ++var++", 0($t2)"
+    Just ([loadAddress] ++ loadindex ++ [accessArray])
+
+
 convertStatement (Assignment ((Token PUNCTUATION "["):(Token SYMBOL name):(Token INTEGER index):(Token PUNCTUATION "]"):[]) [(Token CHAR var)]) = do
     let loadAddress = "la $t0, "++name
         loadindex = ["li $t1, "++index] ++ ["add $t1, $t1, $t1"] ++ ["add $t1, $t1, $t1"] ++ ["add $t2, $t0, $t1"]
